4547ea20a5ed4df7a312f323ac2e1569672ebcf4
Anshuman Aggarwal <anshuman01000001@gmail.com>
2020-08-08 10:06:42 -0400

    Delete ReadFileMain.java

--- ReadFileMain.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JFrame;
-
-public class ReadFileMain{
-	public static void main(String[] args){
-		ReadFile rf = new ReadFile();
-		rf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-}
